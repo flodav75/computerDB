9bec925454358b9b6143f1507e9a3ba88f60613c
flodav75 <fdauvillier@excilys.com>
2019-02-26 17:42:57 +0100

    ajout gitignore

--- src/main/java/fr/excilys/ComputerDataBase/App.java
+++ src/main/java/fr/excilys/ComputerDataBase/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String s1= "animals";
+      
+        System.out.println(s1.indexOf("a",2));
     }
 }
